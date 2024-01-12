a625688c4b3b0effc8fff9f355a166421be5720e
srlitem <santiagorhenals@gmail.com>
2024-01-11 22:44:17 -0500

    Implementing the delegate in the iOS search view

--- lib/ui/pages/landing/landing_page_entry/ios_landing_page.dart
+++ lib/ui/pages/landing/landing_page_entry/ios_landing_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:catbreeds/ui/pages/landing/cat_breeds_list.dart';
+import 'package:catbreeds/ui/pages/landing/search_delegate.dart';
+import 'package:catbreeds/providers/cat_breed_provider.dart';
+import 'package:flutter/material.dart';
 
 class IOSLandingPage extends ConsumerStatefulWidget {
   const IOSLandingPage({Key? key}) : super(key: key);
@@ -27,8 +30,15 @@ class _IOSLandingPageState extends ConsumerState<IOSLandingPage> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('Catbreeds'),
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('Catbreeds'),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: const Icon(CupertinoIcons.search),
+          onPressed: () {
+            showCupertinoSearch(context, ref);
+          },
+        ),
       ),
       child: SafeArea(
         child: Column(
@@ -37,7 +47,9 @@ class _IOSLandingPageState extends ConsumerState<IOSLandingPage> {
               padding: const EdgeInsets.all(8.0),
               child: CupertinoSearchTextField(
                 controller: _searchController,
-                onChanged: (value) => setState(() {}),
+                onChanged: (value) {
+                  setState(() {});
+                },
               ),
             ),
             Expanded(
@@ -49,5 +61,14 @@ class _IOSLandingPageState extends ConsumerState<IOSLandingPage> {
         ),
       ),
     );
+  }
+
+  void showCupertinoSearch(BuildContext context, WidgetRef ref) {
+    ref.read(catBreedProvider.future).then((catBreeds) {
+      showSearch(
+        context: context,
+        delegate: CatBreedSearchDelegate(catBreeds),
+      );
+    });
   }
 }
